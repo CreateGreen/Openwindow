7e9d9bca3131a62cd0d1273fdef1b36d108e6f6d
Jiseok Yong <pqmnvv4@gmail.com>
2023-05-16 22:25:49 +0900

    feat:Add sun component

--- src/components/Skysection.tsx
+++ src/components/Skysection.tsx
@@ -1,11 +1,11 @@
 import styles from '../styles/Skysection.module.css';
-
+import Sun from '../components/view/Sun';
 
 export function Skysection(){
     return (
         <div className={styles.skysection}>
             <div className={styles.container}>
-                skysection 
+                <Sun />
             </div>
         </div>
     )
---
+++ src/components/view/Cloud.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Cloud.module.css';
+
+
+export function Cloud(){
+    return (
+        <div className={styles.cloud}>
+            <div className={styles.container}>
+                Cloud 
+            </div>
+        </div>
+    )
+}
+
+
+export default Cloud
