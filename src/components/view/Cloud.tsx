bf1cc12e9ed7a71da3829acc3b4373ffdb9df74f
Jiseok Yong <pqmnvv4@gmail.com>
2023-05-22 22:15:37 +0900

    feat: Add cloud view (onprocess)

--- src/components/view/Cloud.tsx
+++ src/components/view/Cloud.tsx
@@ -1,11 +1,11 @@
-import styles from '../styles/Cloud.module.css';
-
+import styles from '../../styles/Cloud.module.css';
+import {AiFillCloud} from "react-icons/ai";
 
 export function Cloud(){
     return (
-        <div className={styles.cloud}>
-            <div className={styles.container}>
-                Cloud 
+        <div className={styles.container}>
+            <div className={styles.cloud}>
+                <AiFillCloud className={styles.svg}/>
             </div>
         </div>
     )
